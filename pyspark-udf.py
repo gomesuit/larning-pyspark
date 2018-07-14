3fbbd861d3868dbd14ebe8fdfc4b607033317c39
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-07-14 17:40:08 +0900

    save

---
+++ pyspark-udf.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import udf
+
+sc = SparkContext()
+sqlContext =SQLContext(sc)
+
+maturity_udf = udf(lambda age: "adult" if age >=18 else "child", StringType())
+
+df = sqlContext.createDataFrame([{'name': 'Alice', 'age': 1}])
+df2 = df.withColumn("maturity", maturity_udf(df.age))
+
+df2.printSchema
+df2.show()
