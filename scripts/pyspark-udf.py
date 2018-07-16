44499eec53b9daf8d2cedbd353179202110d733c
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-07-16 11:31:38 +0900

    save

--- scripts/pyspark-udf.py
+++ scripts/pyspark-udf.py
@@ -30,4 +30,71 @@ df_pd = pd.DataFrame(
 df = sqlContext.createDataFrame(df_pd)
 df.printSchema()
 
+df.show()
+
+def square(x):
+    return x**2
+
+
+
+
+
+
+
+# Registering UDF with integer type output
+square_udf_int = udf(lambda z: square(z), IntegerType())
+square_udf_float = udf(lambda z: square(z), FloatType())
+df.select('integers',
+          'floats',
+          square_udf_int('integers').alias('int_squared'),
+          square_udf_int('floats').alias('float_squared')).show()
+
+df.select('integers',
+          'floats',
+          square_udf_float('integers').alias('int_squared'),
+          square_udf_float('floats').alias('float_squared')).show()
+
+def square_float(x):
+    return float(x**2)
+square_udf_float2 = udf(lambda z: square_float(z), FloatType())
+
+df.select('integers',
+          'floats',
+          square_udf_float2('integers').alias('int_squared'),
+          square_udf_float2('floats').alias('float_squared')).show()
+
+def square_list(x):
+    return [float(val)**2 for val in x]
+square_list_udf = udf(lambda y: square_list(y), ArrayType(FloatType()))
+
+df.select('integer_arrays', square_list_udf('integer_arrays')).show()
+
+
+
+
+
+
+# Composite type outputs
+import string
+
+def convert_ascii(number):
+    return [number, string.ascii_letters[number]]
+
+convert_ascii(1)
+
+array_schema = StructType([
+    StructField('number', IntegerType(), nullable=False),
+    StructField('letters', StringType(), nullable=False)
+])
+
+spark_convert_ascii = udf(lambda z: convert_ascii(z), array_schema)
+
+df_ascii = df.select('integers', spark_convert_ascii('integers').alias('ascii_map'))
+df_ascii.show()
+
+
+df_ascii.printSchema()
+
+
+
 embed()
