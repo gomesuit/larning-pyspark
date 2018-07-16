443b0984860e8c30edd5a84443229036242d5d55
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-07-16 10:17:05 +0900

    pandas

--- scripts/pyspark-udf.py
+++ scripts/pyspark-udf.py
@@ -2,6 +2,9 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.sql.functions import udf
+import pandas as pd
+
+from IPython import embed
 
 sc = SparkContext()
 sqlContext =SQLContext(sc)
@@ -13,3 +16,18 @@ df2 = df.withColumn("maturity", maturity_udf(df.age))
 
 df2.printSchema
 df2.show()
+
+
+
+# https://changhsinlee.com/pyspark-udf/
+
+# example data
+df_pd = pd.DataFrame(
+    data={'integers': [1, 2, 3],
+     'floats': [-1.0, 0.5, 2.7],
+     'integer_arrays': [[1, 2], [3, 4, 5], [6, 7, 8, 9]]}
+)
+df = sqlContext.createDataFrame(df_pd)
+df.printSchema()
+
+embed()
